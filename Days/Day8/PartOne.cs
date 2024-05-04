1729b5df38d8df5bf73c9f55023da0eeb55704e3
Kevin Nash <nashkevin@live.com>
2024-05-04 16:01:04 -0400

    Solve D8P1

---
+++ Days/Day8/PartOne.cs
@@ -0,0 +1,33 @@
+﻿using static Days.Day8.Shared;
+
+namespace Days.Day8
+{
+    public static class PartOne
+    {
+        public static int SolveFromFile(string path)
+        {
+            return Solve(File.ReadAllLines(path));
+        }
+
+        public static int Solve(string[] s)
+        {
+            Dictionary<string, Node> nodes = [];
+            Node node;
+            for (int i = 2; i < s.Length; i++)
+            {
+                node = new(s[i]);
+                nodes.Add(node.Self, node);
+            }
+            node = nodes["AAA"];
+            int index = 0;
+            int stepCounter = 0;
+            while (node.Self != "ZZZ")
+            {
+                node = s[0][index] == 'L' ? nodes[node.Left] : nodes[node.Right];
+                index = index < s[0].Length - 1 ? index + 1 : 0;
+                stepCounter++;
+            }
+            return stepCounter;
+        }
+    }
+}
---
+++ Days/Day8/Shared.cs
@@ -0,0 +1,35 @@
+﻿namespace Days.Day8
+{
+    internal class Shared
+    {
+        internal class Node
+        {
+            public string Self { get; set; }
+            public string Left { get; set; }
+            public string Right { get; set; }
+
+            public Node(string s)
+            {
+                string[] splits = s.Split(" = ");
+                Self = splits[0];
+                splits = splits[1][1..^1].Split(", ");
+                Left = splits[0];
+                Right = splits[1];
+            }
+
+            public Node(Node other)
+            {
+                Self = other.Self;
+                Left = other.Left;
+                Right = other.Right;
+            }
+
+            public Node(string self, string left, string right)
+            {
+                Self = self;
+                Left = left;
+                Right = right;
+            }
+        }
+    }
+}
